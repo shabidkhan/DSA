a99bf84c0b0789c58df0d6a74519a9c0cbfedf1b
Shabid Khan <shabid19@navgurukul.org>
2026-06-27 14:16:08 +0530

    Add Reverse Vowels of a String solution with Python

---
+++ 01-Array/01-Two-pointer/01-Opposite-ends/Python/ReverseVowelsString.py
@@ -0,0 +1,24 @@
+# 345. Reverse Vowels of a String
+# https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+
+def reverseVowels(self, s: str) -> str:
+    start = 0
+    end = len(s) - 1
+    vowels = 'AaEeIiOoUu'
+    arrayStr = list(s)
+    while (start < end):
+        if (s[start] not in vowels and s[end] not in vowels):
+            start += 1
+            end -= 1
+            continue
+        if(s[start] not in vowels):
+            start += 1
+            continue
+        if(s[end] not in vowels):
+            end -= 1
+            continue
+        arrayStr[start], arrayStr[end] = arrayStr[end], arrayStr[start]
+        start += 1
+        end -= 1
+    return ''.join(arrayStr)
+    
